7cd022026f56bed0fcddc36cb128e0e8a6bb514e
pouya nicholas khast <nicholas.khast@gmail.com>
2017-05-04 12:34:57 +0200

    modified starter page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AlertModule.forRoot(),
     FormsModule,
     HttpModule
   ],
